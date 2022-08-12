2dccece5616cafb0a9db22e5b1176fbb99998097
JeremyC <jeremy4322@gmail.com>
2022-08-12 00:46:02 -0400

    moved files to desktop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import firebase from './firebase.js'
 import Header from './Header.js'
 import Form from './Form.js'
 
 
 function App() {
+  const [wasteData, setWasteData] = useState([]);
+
   return (
     <div className="App">
       <Header />
       <Form />
+      <div>
+        {wasteDataResponse.map((wasteData) => {
+          
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXyZwl3PF46F_gU2J_8_-Xb-GhJviyBUU",
+  authDomain: "project3-b8921.firebaseapp.com",
+  projectId: "project3-b8921",
+  storageBucket: "project3-b8921.appspot.com",
+  messagingSenderId: "620945466652",
+  appId: "1:620945466652:web:f8680abfbb9442c279c6d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
